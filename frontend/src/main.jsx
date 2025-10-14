02901327cd4c34f7776cbd8f3768fd238883d17f
PiyumalSandeepa <mkpiyumalsandeepa@gmail.com>
2025-10-14 22:00:46 +0530

    Fix Maintainabvilty issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import LandingPage from './pages/LandingPage.jsx'
-import TaskAddPage from './pages/TaskAddPage.jsx'
-import TaskListPage from './pages/TaskListPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
